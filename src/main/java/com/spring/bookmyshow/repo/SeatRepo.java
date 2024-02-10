8c22f9f466bf4def92c0ad79904e044b59a0b778
HEMANATHAN21 <hemanath.m2002@gmail.com>
2024-02-10 12:28:56 +0530

    SeatRepo Created

---
+++ src/main/java/com/spring/bookmyshow/repo/SeatRepo.java
@@ -0,0 +1,10 @@
+package com.spring.bookmyshow.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.spring.bookmyshow.entity.Seat;
+
+public interface SeatRepo extends JpaRepository<Seat, String> 
+{
+
+}
