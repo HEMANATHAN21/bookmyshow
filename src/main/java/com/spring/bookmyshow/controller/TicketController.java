ae34ef8e1f625e5c4351df859c9ffc57940c4db4
HEMANATHAN21 <hemanath.m2002@gmail.com>
2024-02-10 11:37:33 +0530

    TicketController Created

---
+++ src/main/java/com/spring/bookmyshow/controller/TicketController.java
@@ -0,0 +1,49 @@
+package com.spring.bookmyshow.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.bookmyshow.entity.Screen;
+import com.spring.bookmyshow.entity.Ticket;
+import com.spring.bookmyshow.service.TicketService;
+import com.spring.bookmyshow.util.ResponseStructure;
+
+@RestController
+@RequestMapping("ticket")
+public class TicketController 
+{
+	@Autowired
+	TicketService ticketService;
+	
+	@PostMapping
+	public ResponseEntity<ResponseStructure<Ticket>> saveTicket(@RequestBody Ticket ticket)
+	{
+		return ticketService.saveTicket(ticket);
+	}
+	
+	@GetMapping
+	public ResponseEntity<ResponseStructure<Ticket>> findTicket(@RequestParam int ticketId)
+	{
+		return ticketService.findTicket(ticketId);
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<ResponseStructure<Ticket>> deleteTicket(@RequestParam int ticketId)
+	{
+		return ticketService.deleteTicket(ticketId);
+	}
+	
+	@PutMapping
+	public ResponseEntity<ResponseStructure<Screen>> updateScreen(@RequestBody Screen screen,@RequestParam int screenId)
+	{
+		return screenService.updateScreen(screen,screenId);
+	}
+}
