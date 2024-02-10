21ddf445fe33233c65b70d2f955aa6b5bd859b60
HEMANATHAN21 <hemanath.m2002@gmail.com>
2024-02-10 20:00:53 +0530

    SeatController Created

--- src/main/java/com/spring/bookmyshow/service/SeatService.java
+++ src/main/java/com/spring/bookmyshow/service/SeatService.java
@@ -1,5 +1,84 @@
 package com.spring.bookmyshow.service;
 
-public class SeatService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+import com.spring.bookmyshow.dao.SeatDao;
+import com.spring.bookmyshow.entity.Seat;
+import com.spring.bookmyshow.util.ResponseStructure;
+
+@Service
+public class SeatService 
+{
+	@Autowired
+	SeatDao seatDao;
+	
+	public ResponseEntity<ResponseStructure<Seat>> saveSeat(Seat seat)
+	{
+		Seat seatNew = seatDao.saveSeat(seat);
+		if(seatNew != null)
+		{
+			ResponseStructure<Seat> structure = new ResponseStructure<>();
+			structure.setMessage("Seat Saved");
+			structure.setStatus(HttpStatus.CREATED.value());
+			structure.setData(seatNew);
+			return new ResponseEntity<ResponseStructure<Seat>>(structure,HttpStatus.CREATED);
+		}
+		return null;
+	}
+	
+	public ResponseEntity<ResponseStructure<Seat>> findSeat(int seatId)
+	{
+		Seat seatfind = seatDao.findSeat(seatId);
+		if(seatfind != null)
+		{
+			ResponseStructure<Seat> structure = new ResponseStructure<>();
+			structure.setMessage("Seat Found");
+			structure.setStatus(HttpStatus.FOUND.value());
+			structure.setData(seatfind);
+			return new ResponseEntity<ResponseStructure<Seat>>(structure,HttpStatus.FOUND);
+		}
+		return null;
+	}
+	
+	public ResponseEntity<ResponseStructure<Seat>> deleteseat(int seatId)
+	{
+		Seat seat = seatDao.findSeat(seatId);
+		if(seat != null)
+		{
+			Seat deletedSeat = seatDao.deleteSeat(seatId);
+			if(deletedSeat != null)
+			{
+				ResponseStructure<Seat> structure = new ResponseStructure<>();
+				structure.setMessage("Seat Deleted");
+				structure.setStatus(HttpStatus.OK.value());
+				structure.setData(deletedSeat);
+				return new ResponseEntity<ResponseStructure<Seat>>(structure,HttpStatus.OK);
+			}
+			return null;
+		}
+		return null;
+	}
+	
+	public ResponseEntity<ResponseStructure<Seat>> updateSeat(Seat seat, int seatId)
+	{
+		Seat exSeat = seatDao.findSeat(seatId);
+		if(exSeat != null)
+		{
+			Seat updatedSeat = seatDao.updateSeat(seat, seatId);
+			if(updatedSeat != null)
+			{
+				ResponseStructure<Seat> structure = new ResponseStructure<>();
+				structure.setMessage("Seat Updated");
+				structure.setStatus(HttpStatus.OK.value());
+				structure.setData(updatedSeat);
+				return new ResponseEntity<ResponseStructure<Seat>>(structure,HttpStatus.OK);
+				
+			}
+			return null;
+		}
+		return null;
+	}
 }
