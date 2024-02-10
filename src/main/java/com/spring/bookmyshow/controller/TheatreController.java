d6d6d51a55bddcbb735b8913d0ec2a090e374124
HEMANATHAN21 <hemanath.m2002@gmail.com>
2024-02-10 10:26:56 +0530

    TheatreController added

---
+++ src/main/java/com/spring/bookmyshow/controller/TheatreController.java
@@ -0,0 +1,47 @@
+package com.spring.bookmyshow.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.spring.bookmyshow.entity.Theatre;
+import com.spring.bookmyshow.service.TheatreService;
+import com.spring.bookmyshow.util.ResponseStructure;
+
+@RestController
+@RequestMapping("theatre")
+public class TheatreController 
+{
+	@Autowired
+	TheatreService theatreService;
+	
+	@PostMapping
+	public ResponseEntity<ResponseStructure<Theatre>> saveTheatre(@RequestBody Theatre theatre)
+	{
+		return theatreService.saveTheatre(theatre);
+	}
+	
+	@GetMapping
+	public ResponseEntity<ResponseStructure<Theatre>> findTheatre(@RequestParam int theatreId)
+	{
+		return theatreService.findTheatre(theatreId);
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<ResponseStructure<Theatre>> deleteTheatre(@RequestParam int theatreId)
+	{
+		return theatreService.deleteTheatre(theatreId);
+	}
+	
+	@PutMapping
+	public ResponseEntity<ResponseStructure<Theatre>> updateTheatre(@RequestBody Theatre theatre,@RequestParam int theatreId)
+	{
+		return theatreService.updateTheatre(theatre,theatreId);
+	}
+}
--- src/main/java/com/spring/bookmyshow/dao/TheatreDao.java
+++ src/main/java/com/spring/bookmyshow/dao/TheatreDao.java
@@ -1,5 +1,52 @@
 package com.spring.bookmyshow.dao;
 
-public class TheatreDao {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import com.spring.bookmyshow.entity.Theatre;
+import com.spring.bookmyshow.repo.TheatreRepo;
+
+@Repository
+public class TheatreDao 
+{
+	@Autowired
+	TheatreRepo theatreRepo;
+	
+	public Theatre saveTheatre(Theatre theatre)
+	{
+		return theatreRepo.save(theatre);
+	}
+	
+	public Theatre findTheatre(int theatreId)
+	{
+		Optional<Theatre> opTheatre = theatreRepo.findById(theatreId);
+		if(opTheatre.isPresent())
+			return opTheatre.get();
+		return null;
+	}
+	
+	public Theatre deleteTheatre(int theatreId)
+	{
+		Theatre theatre = findTheatre(theatreId);
+		theatreRepo.delete(theatre);
+		return theatre;
+	}
+	
+	public Theatre updateTheatre(Theatre theatre, int theatreId)
+	{
+		Theatre exTheatre = findTheatre(theatreId);
+		if(exTheatre != null)
+		{
+			exTheatre.setTheatreId(theatreId);
+			return theatreRepo.save(theatre);
+		}
+		return null;
+	}
+	
+	public List<Theatre> findAllTheatre()
+	{
+		return theatreRepo.findAll();
+	}
 }
